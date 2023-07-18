70f4fe7414840534b1ef917add47459d810aa1ec
Bharathram1256 <maruboyinibharathkumar@gmail.com>
2023-07-18 11:06:05 +0530

    hashing

---
+++ hashing.java
@@ -0,0 +1,16 @@
+public class hashing {
+    public static int customHashFunction(String input) {
+        int hashCode = 0;
+        for(int i=0;i<input.length();i++){
+            hashCode +=  input.charAt(i);
+
+        }        
+        return hashCode;
+    }
+    public static void main(String[] args) {
+        String name = "Bharath";
+        int hashCode = customHashFunction(name);
+        System.out.println(name);
+        System.out.println(hashCode);
+    }
+}
